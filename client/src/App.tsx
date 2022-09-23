f00ce70a9c5a4d97d936df0635398aae9044999b
Chris Akiki <akikichriss@gmail.com>
2022-09-23 16:45:47 +0300

    initial p

---
+++ client/src/App.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="container mx-auto max-w-6xl text-center drop-shadow-lg text-gray-800">
+        <h1 className="text-3xl">Expense Tracker</h1>
+      </div>
+    </div>
+  );
+}
+
+export default App;
